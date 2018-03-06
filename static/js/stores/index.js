07e99b747c6aae0d11c2637ee9de78e2ff40c672
Vinicius Isola <viniciusisola@gmail.com>
2018-03-06 09:45:38 -0500

    Improve admin UI to show graph

--- static/js/stores/index.js
+++ static/js/stores/index.js
@@ -1,8 +1,12 @@
 import Configurations from './Configurations';
+import ProxiedRequests from './ProxiedRequests';
 
 const configurations = new Configurations();
 configurations.fetch();
 
+const proxiedRequests = new ProxiedRequests();
+
 export default {
   configurations,
+  proxiedRequests,
 };
