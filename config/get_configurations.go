3f8b3108cd4a0939dc0cbb79425a1fdfaa958ebe
Vinicius Isola <viniciusisola@gmail.com>
2018-03-12 10:30:43 -0400

    Add ID to mappings and refactor config loading

--- config/get_configurations.go
+++ config/get_configurations.go
@@ -2,6 +2,7 @@ package config
 
 import (
 	"io/ioutil"
+	"os"
 	"os/user"
 	"path"
 	"path/filepath"
@@ -13,12 +14,26 @@ import (
 
 var configurations []Mapping
 
-// GetConfigurations load all configurations.
+// GetConfigurations get all configurations or load them if not loaded so far
 func GetConfigurations() ([]Mapping, error) {
 	if configurations != nil {
 		return configurations, nil
 	}
 
+	var err error
+	configurations, err = loadAllConfigurations()
+	return configurations, err
+}
+
+func getConfigDirectory() (string, error) {
+	user, err := user.Current()
+	if err != nil {
+		return "", err
+	}
+	return user.HomeDir + "/.go-proxy", nil
+}
+
+func loadAllConfigurations() ([]Mapping, error) {
 	configDir, configDirErr := getConfigDirectory()
 	if configDirErr != nil {
 		panic(configDirErr)
@@ -30,46 +45,38 @@ func GetConfigurations() ([]Mapping, error) {
 	}
 
 	configurations = make([]Mapping, 0)
-
 	for _, file := range files {
 		if filepath.Ext(file.Name()) != ".yml" && filepath.Ext(file.Name()) != ".yaml" {
 			continue
 		}
 
-		config, configErr := readConfiguration(path.Join(configDir, file.Name()))
-		if configErr != nil {
-			return nil, configErr
+		loadedConfigurations, loadingErr := loadConfigurations(configDir, file)
+		if loadingErr != nil {
+			return nil, loadingErr
 		}
-
-		for _, staticConfig := range config.Static {
-			configurations = append(configurations, Mapping{
-				From:   staticConfig.From,
-				To:     staticConfig.To,
-				Origin: file.Name(),
-				Proxy:  false,
-			})
-		}
-
-		for _, staticConfig := range config.Proxy {
-			configurations = append(configurations, Mapping{
-				From:   staticConfig.From,
-				To:     staticConfig.To,
-				Origin: file.Name(),
-				Proxy:  true,
-			})
-		}
+		configurations = append(configurations, loadedConfigurations...)
 	}
 
-	sort.Slice(configurations, func(i, j int) bool {
-		pathI := strings.ToLower(configurations[i].From)
-		pathJ := strings.ToLower(configurations[j].From)
+	sortConfigurations(configurations)
+	return configurations, nil
+}
 
-		if len(pathI) == len(pathJ) {
-			return strings.Compare(pathI, pathJ) < 0
-		}
+func loadConfigurations(configDir string, file os.FileInfo) ([]Mapping, error) {
+	configurations = make([]Mapping, 0)
 
-		return len(pathI) > len(pathJ)
-	})
+	config, configErr := readConfiguration(path.Join(configDir, file.Name()))
+	if configErr != nil {
+		return nil, configErr
+	}
+
+	for _, staticConfig := range config.Static {
+		configurations = append(configurations, fromYAMLMapping(staticConfig, file.Name(), false))
+	}
+
+	for _, staticConfig := range config.Proxy {
+		configurations = append(configurations, fromYAMLMapping(staticConfig, file.Name(), true))
+	}
+
 	return configurations, nil
 }
 
@@ -85,10 +92,15 @@ func readConfiguration(file string) (loadedConfig proxyConfig, err error) {
 	return loadedConfig, err
 }
 
-func getConfigDirectory() (string, error) {
-	user, err := user.Current()
-	if err != nil {
-		return "", err
-	}
-	return user.HomeDir + "/.go-proxy", nil
+func sortConfigurations(configurations []Mapping) {
+	sort.Slice(configurations, func(i, j int) bool {
+		pathI := strings.ToLower(configurations[i].From)
+		pathJ := strings.ToLower(configurations[j].From)
+
+		if len(pathI) == len(pathJ) {
+			return strings.Compare(pathI, pathJ) < 0
+		}
+
+		return len(pathI) > len(pathJ)
+	})
 }
--- config/models.go
+++ config/models.go
@@ -1,9 +1,35 @@
 package config
 
+import (
+	"crypto/sha1"
+	"encoding/base64"
+)
+
 // Mapping represents a mapping configuration loaded from some file.
 type Mapping struct {
-	From   string `json:"from"`
-	Origin string `json:"origin"`
-	Proxy  bool   `json:"proxy"`
-	To     string `json:"to"`
+	Active    bool   `json:"active"`
+	From      string `json:"from"`
+	MappingID string `json:"mappingID"`
+	Origin    string `json:"origin"`
+	Proxy     bool   `json:"proxy"`
+	To        string `json:"to"`
+}
+
+// fromYAMLMapping creates a new mapping from a mapping loaded from a YAML config
+func fromYAMLMapping(loaded mapping, origin string, proxy bool) Mapping {
+	newMapping := Mapping{
+		From:   loaded.From,
+		To:     loaded.To,
+		Origin: origin,
+		Proxy:  proxy,
+	}
+
+	newMapping.MappingID = generateID(newMapping)
+	return newMapping
+}
+
+func generateID(mapping Mapping) string {
+	hasher := sha1.New()
+	hasher.Write([]byte(mapping.Origin + mapping.From + mapping.To))
+	return base64.URLEncoding.EncodeToString(hasher.Sum(nil))
 }
