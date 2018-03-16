63dae897c6e895119b32f6ee0ccfdff42cb22adb
Vinicius Isola <viniciusisola@gmail.com>
2018-03-16 17:33:12 -0400

    No need for these headers

--- http/helpers.go
+++ http/helpers.go
@@ -45,7 +45,6 @@ func NotFound(req *http.Request, w http.ResponseWriter, path string) {
 	}
 
 	w.WriteHeader(http.StatusNotFound)
-	w.Header().Set("Content-Type", "text/plain")
 	w.Write([]byte(fmt.Sprintf("Sorry, nothing here for: '%s'\n", path)))
 }
 
@@ -54,6 +53,5 @@ func NotFound(req *http.Request, w http.ResponseWriter, path string) {
 func InternalError(req *http.Request, w http.ResponseWriter, err error) {
 	fmt.Printf("Error: %s\n", err)
 	w.WriteHeader(http.StatusInternalServerError)
-	w.Header().Set("Content-Type", "text/plain")
 	w.Write([]byte(fmt.Sprintf("Sorry, something went wrong: '%s'\n", err)))
 }
