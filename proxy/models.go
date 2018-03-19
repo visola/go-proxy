9b5688ba71a5b748f8f2637a27e17f32eeec7bf1
Vinicius Isola <viniciusisola@gmail.com>
2018-03-19 10:35:56 -0400

    Sort models by name

--- proxy/models.go
+++ proxy/models.go
@@ -4,17 +4,17 @@ import (
 	"bytes"
 )
 
-type proxyResponse struct {
-	body         string
-	executedURL  string
-	headers      map[string][]string
-	responseCode int
-}
-
 type closeableByteBuffer struct {
 	*bytes.Buffer
 }
 
 func (bb closeableByteBuffer) Close() error {
 	return nil
+}
+
+type proxyResponse struct {
+	body         string
+	executedURL  string
+	headers      map[string][]string
+	responseCode int
 }
