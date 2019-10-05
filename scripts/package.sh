e6ff1e472f5588afc90ac88c5542ff9f99001f0b
Vinicius Isola <viniciusisola@gmail.com>
2019-10-04 23:12:08 -0400

    Update build and package scripts

--- scripts/package.sh
+++ scripts/package.sh
@@ -1,23 +1,19 @@
 #!/bin/bash
 set -e
 
-npm install
-npm run bundle
-
-go get -u github.com/gobuffalo/packr/...
-packr clean
-packr
+BASE_PACKAGE=build/packages
+mkdir -p $BASE_PACKAGE
 
 build_and_zip() {
   # $1 -> operating system
   # $2 -> architecture
   # $3 -> OS alias, used in the output file name
   # $4 -> Optional extension with ".", e.g.: .exe
-  PACKAGE_DIR=build/$3_$2
+  PACKAGE_DIR=$BASE_PACKAGE/$3_$2
   PACKAGE_FILE=$PACKAGE_DIR/go-proxy$4
   mkdir $PACKAGE_DIR
-  GOOS=$1 GOARCH=$2 go build -o $PACKAGE_FILE
-  zip -j build/$3_$2.zip $PACKAGE_FILE LICENSE
+  GOOS=$1 GOARCH=$2 go build -o $PACKAGE_FILE ./cmd/go-proxy
+  zip -j $BASE_PACKAGE/$3_$2.zip $PACKAGE_FILE LICENSE
   rm -Rf $PACKAGE_DIR
 }
 
