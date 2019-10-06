b715b52602ed5d7c651169740767bfbdf46390a8
Vinicius Isola <viniciusisola@gmail.com>
2019-10-05 20:24:55 -0400

    chore(build): add go version to final package

--- scripts/package.sh
+++ scripts/package.sh
@@ -2,7 +2,12 @@
 set -e
 
 BASE_PACKAGE=build/packages
+rm -Rf $BASE_PACKAGE
 mkdir -p $BASE_PACKAGE
+
+ENVIRONMENT_FILE=build/go-version.txt
+rm -f $ENVIRONMENT_FILE
+go version >> $ENVIRONMENT_FILE
 
 build_and_zip() {
   # $1 -> operating system
@@ -13,7 +18,7 @@ build_and_zip() {
   PACKAGE_FILE=$PACKAGE_DIR/go-proxy$4
   mkdir $PACKAGE_DIR
   GOOS=$1 GOARCH=$2 go build -o $PACKAGE_FILE ./cmd/go-proxy
-  zip -j $BASE_PACKAGE/$3_$2.zip $PACKAGE_FILE LICENSE
+  zip -j $BASE_PACKAGE/$3_$2.zip $PACKAGE_FILE LICENSE $ENVIRONMENT_FILE
   rm -Rf $PACKAGE_DIR
 }
 
