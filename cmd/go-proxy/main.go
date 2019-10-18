8b8a0a1d5846c8beba440de61df3f0320a3cc2b4
Vinicius Isola <viniciusisola@gmail.com>
2019-10-18 13:38:18 -0400

    refactor(*): remove all old files

--- cmd/go-proxy/main.go
+++ cmd/go-proxy/main.go
@@ -2,71 +2,8 @@ package main
 
 import (
 	"log"
-	"os"
-	"strconv"
-
-	"github.com/visola/go-proxy/pkg/configuration"
-	"github.com/visola/go-proxy/pkg/mapping"
-
-	"github.com/visola/go-proxy/pkg/admin"
-	"github.com/visola/go-proxy/pkg/proxy"
 )
 
 func main() {
-	initializeEnvironment()
-
-	mapping.Initialize()
-
-	go startAdmin()
-	startProxy()
-}
-
-func initializeEnvironment() {
-	envOptions := make([]configuration.EnvironmentOption, 0)
-
-	certFile := os.Getenv("GO_PROXY_CERT_FILE")
-	if certFile != "" {
-		envOptions = append(envOptions, configuration.WithCertificateFile(certFile))
-	}
-
-	keyFile := os.Getenv("GO_PROXY_CERT_KEY_FILE")
-	if keyFile != "" {
-		envOptions = append(envOptions, configuration.WithKeyFile(keyFile))
-	}
-
-	adminPort := os.Getenv("GO_PROXY_ADMIN_PORT")
-	if adminPort != "" {
-		if port, err := strconv.Atoi(adminPort); err == nil {
-			envOptions = append(envOptions, configuration.WithAdminPort(port))
-		} else {
-			log.Fatal("Invalid admin port, not a number: " + adminPort)
-		}
-	}
-
-	proxyPort := os.Getenv("GO_PROXY_PORT")
-	if proxyPort != "" {
-		if port, err := strconv.Atoi(proxyPort); err == nil {
-			envOptions = append(envOptions, configuration.WithProxyPort(port))
-		} else {
-			log.Fatal("Invalid proxy port, not a number: " + proxyPort)
-		}
-	}
-
-	configuration.InitializeEnvironment(envOptions...)
-}
-
-func startAdmin() {
-	log.Println("Starting admin server...")
-	adminError := admin.StartAdminServer()
-	if adminError != nil {
-		panic(adminError)
-	}
-}
-
-func startProxy() {
-	log.Println("Starting proxy server...")
-	proxyError := proxy.StartProxyServer()
-	if proxyError != nil {
-		panic(proxyError)
-	}
+	log.Print("Initializing go-proxy...")
 }
--- pkg/mapping/initialize.go
+++
@@ -1,9 +0,0 @@
-package mapping
-
-// Initialize will load the last saved state and start the thread pools to
-// refresh the mappings
-func Initialize() {
-	// TODO - Load current state
-	// TODO - Initialize thread pool to refresh mappings
-	// TODO - Initialize thread pool to walk base directories
-}
