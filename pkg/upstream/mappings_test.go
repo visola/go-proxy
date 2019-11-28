c44effbd633935cc043a4ec5e5e0045ee27d82e0
Vinicius Isola <viniciusisola@gmail.com>
2019-11-28 11:02:36 -0500

    chore(upstream): add tests for matching algorithm

---
+++ pkg/upstream/mappings_test.go
@@ -0,0 +1,40 @@
+package upstream
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMatches(t *testing.T) {
+	t.Run("Doesn't match wrong path", testDoesntMatch)
+	t.Run("Matches by from", testMatchesByFrom)
+	t.Run("Matches by regexp", testMatchesByRegexp)
+}
+
+func testDoesntMatch(t *testing.T) {
+	m := Mapping{}
+	req := httptest.NewRequest(http.MethodGet, "http://nowhere.com/another", strings.NewReader("Some Body"))
+	assert.Equal(t, false, m.Matches(*req))
+}
+
+func testMatchesByFrom(t *testing.T) {
+	m := Mapping{
+		From: "/test",
+	}
+
+	req := httptest.NewRequest(http.MethodGet, "http://nowhere.com/test", strings.NewReader("Some Body"))
+	assert.Equal(t, true, m.Matches(*req))
+}
+
+func testMatchesByRegexp(t *testing.T) {
+	m := Mapping{
+		Regexp: "/.*/more/(.*)",
+	}
+
+	req := httptest.NewRequest(http.MethodGet, "http://nowhere.com/test/more/complex", strings.NewReader("Some Body"))
+	assert.Equal(t, true, m.Matches(*req))
+}
