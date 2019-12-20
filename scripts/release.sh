6e74121fbfd1c377c2dfdd2677d25439f0cacecf
Vinicius Isola <vinicius.isola@toasttab.com>
2019-12-20 18:05:39 -0500

    chore(build): fix release script to use travisci.com

--- scripts/release.sh
+++ scripts/release.sh
@@ -3,7 +3,7 @@
 set -e
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-./semantic-release --token $GITHUB_TOKEN --slug visola/go-proxy --ghr --vf
+./semantic-release -travis-com --token $GITHUB_TOKEN --slug visola/go-proxy --ghr --vf
 export VERSION=$(cat .version)
 
 $SCRIPT_DIR/package.sh
