2afea03ba5fa197b1df9695d5a20585a463ad478
Vinicius Isola <viniciusisola@gmail.com>
2019-12-25 20:21:22 -0500

    refactor(admin): listeners/upstreams endpoint receives an array of upstream names

--- pkg/admin/listener_endpoints.go
+++ pkg/admin/listener_endpoints.go
@@ -1,40 +1,50 @@
 package admin
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"strconv"
 
 	"github.com/gorilla/mux"
-	"github.com/visola/go-proxy/pkg/configuration"
 	"github.com/visola/go-proxy/pkg/httputil"
 	"github.com/visola/go-proxy/pkg/listener"
 	"github.com/visola/go-proxy/pkg/upstream"
 )
 
 type UpstreamStateChangeResult struct {
-	Listener listener.Listener `json:"listener"`
-	Upstream upstream.Upstream `json:"upstream"`
+	Listener listener.Listener   `json:"listener"`
+	Upstream []upstream.Upstream `json:"upstreams"`
 }
 
 func registerListenerEndpoints(router *mux.Router) {
 	router.HandleFunc("/listeners", getListeners).Methods(http.MethodGet)
-	router.HandleFunc("/listeners/{listenerPort}/upstreams/{upstreamName}", enableUpstream).Methods(http.MethodPut)
+	router.HandleFunc("/listeners/{listenerPort}/upstreams", enableUpstream).Methods(http.MethodPut)
 }
 
 func enableUpstream(resp http.ResponseWriter, req *http.Request) {
+	decoder := json.NewDecoder(req.Body)
+	var upstreamNames []string
+	decodeErr := decoder.Decode(&upstreamNames)
+	if decodeErr != nil {
+		httputil.InternalError(req, resp, decodeErr)
+		return
+	}
+
 	listenerPort, portError := strconv.Atoi(mux.Vars(req)["listenerPort"])
 	if portError != nil {
 		httputil.InternalError(req, resp, portError)
 		return
 	}
 
-	upstreamName := mux.Vars(req)["upstreamName"]
-
-	upstreamFound, ok := upstream.Upstreams()[upstreamName]
-	if !ok {
-		httputil.NotFound(req, resp, fmt.Sprintf("Upstrem not found: %s", upstreamName))
-		return
+	loadedUpstreams := make([]upstream.Upstream, 0)
+	for _, upstreamName := range upstreamNames {
+		loadedUpstream, ok := upstream.Upstreams()[upstreamName]
+		if !ok {
+			httputil.NotFound(req, resp, fmt.Sprintf("Upstrem not found: %s", upstreamName))
+			return
+		}
+		loadedUpstreams = append(loadedUpstreams, loadedUpstream)
 	}
 
 	if _, ok := listener.Listeners()[listenerPort]; !ok {
@@ -42,14 +52,11 @@ func enableUpstream(resp http.ResponseWriter, req *http.Request) {
 		return
 	}
 
-	listener.SetUpstreamState(listenerPort, upstreamName, true)
-	if err := configuration.SaveToPersistedState(); err != nil {
-		httputil.InternalError(req, resp, err)
-	}
+	listener.SetEnabledUpstreams(listenerPort, upstreamNames)
 
 	result := UpstreamStateChangeResult{
 		Listener: listener.Listeners()[listenerPort],
-		Upstream: upstreamFound,
+		Upstream: loadedUpstreams,
 	}
 
 	httputil.RespondWithJSON(result, resp, req)
