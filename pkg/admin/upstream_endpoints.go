24118874dbbf6418a294cc6b678354201d73009f
Vinicius Isola <viniciusisola@gmail.com>
2019-12-30 08:30:27 -0500

    feat(admin): add get upstreams endpoint

---
+++ pkg/admin/upstream_endpoints.go
@@ -0,0 +1,26 @@
+package admin
+
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/visola/go-proxy/pkg/httputil"
+	"github.com/visola/go-proxy/pkg/upstream"
+)
+
+func registerUpstreamEndpoints(router *mux.Router) {
+	router.HandleFunc("/upstreams", getUpstreams).Methods(http.MethodGet)
+}
+
+func getUpstreams(resp http.ResponseWriter, req *http.Request) {
+	upstreamsMap := upstream.Upstreams()
+	upstreamsArray := make([]upstream.Upstream, len(upstreamsMap))
+
+	index := 0
+	for _, l := range upstreamsMap {
+		upstreamsArray[index] = l
+		index++
+	}
+
+	httputil.RespondWithJSON(upstreamsArray, resp, req)
+}
