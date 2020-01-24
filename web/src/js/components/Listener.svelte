d364b99604cc94268d7f4066ad9512111421962c
Vinicius Isola <viniciusisola@gmail.com>
2020-01-24 15:32:57 -0500

    fix(frontend): fix typo for http/s listeners

--- web/src/js/components/Listener.svelte
+++ web/src/js/components/Listener.svelte
@@ -21,7 +21,7 @@ function isHttps(listener) {
     <a href={getUrl(listener)} target="_blank"><i class="external alternate icon"></i></a>
     <div class="ui label large">Port<div class="detail">{listener.port}</div></div>
     <div class="ui label large {isHttps(listener) ? 'green' : 'orange'}">
-      HTTPS
+      {isHttps(listener) ? 'HTTPS' : 'HTTP'}
       <div class="detail"><i class="{isHttps(listener) ? 'lock' : 'unlock'} alternate icon"></i></div>
     </div>
   </div>
